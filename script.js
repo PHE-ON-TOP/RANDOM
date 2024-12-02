d9a7243be6c2aab8cf5cb872aaa5f1f4975578b5
PHOOMPHAT <phomphat2011@gmail.com>
2024-12-02 19:20:21 +0700

    script.js

--- script.js
+++ script.js
@@ -68,4 +68,20 @@ document.getElementById("drawButton").addEventListener("click", function() {
     }
   
     showSlot(currentSlot);
-  }  
+  }
+
+  document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+        e.preventDefault();
+    }
+});
+
+document.addEventListener('mousedown', function(e) {
+    if (e.button === 0) {
+        e.preventDefault();
+    }
+});
